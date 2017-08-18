c701fd1188c6a83154a2e081914bc2947196022c
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-08-18 13:51:41 +0300

    added new components and providers in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutesModule } from './routes.module';
 
@@ -9,22 +11,43 @@ import { ActivitiesModule } from './modules/fitness/activities.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './modules/nav/nav.component';
 import { HomeComponent } from './modules/home/home.component';
+import { SignUpFormComponent } from './modules/user/signUp-form/signUp-form.component';
+import { SignInFormComponent } from './modules/user/signIn-form/signIn-form.component';
 
 import { DataService } from './services/data.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
+import { firebaseConfig } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    SignUpFormComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutesModule,
     RecipesModule,
     ActivitiesModule
   ],
-  providers: [DataService],
+  providers: [
+    DataService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
