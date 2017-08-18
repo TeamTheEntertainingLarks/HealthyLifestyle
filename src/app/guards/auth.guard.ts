e831d784292e34af40eb00d06d8e6593db2c25e5
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-08-18 13:45:49 +0300

    authentication guard added for not authenticated users.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { AuthService } from './../services/auth.service';
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    auth: AuthService;
+    constructor(private router: Router, auth: AuthService) {
+        this.auth = auth;
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (this.auth.currentUser) {
+            // logged in so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
