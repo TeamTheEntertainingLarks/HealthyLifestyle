84bd4b975a67d2a4ef58c9c44c6f779a860f1766
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-08-21 00:16:45 +0300

    firebaseConfig removed

--- src/environments/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyDORMqI9tRjPWWOXbJJFkuKgLtnOQrU-ic',
-    authDomain: 'healthylifestyle-f4c1d.firebaseapp.com',
-    databaseURL: 'https://healthylifestyle-f4c1d.firebaseio.com',
-    projectId: 'healthylifestyle-f4c1d',
-    storageBucket: 'healthylifestyle-f4c1d.appspot.com',
-    messagingSenderId: '970960030257'
-  };
