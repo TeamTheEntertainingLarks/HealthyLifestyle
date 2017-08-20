2db2ae38a5d135de2812577d9bd0a1846110ee12
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-08-20 18:38:23 +0300

    recipe-data.service added

---
+++ src/app/services/recipe-data.service.ts
@@ -0,0 +1,50 @@
+import { Recipe } from './../models/recipe';
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { RecipeInterface } from '../interfaces/recipe';
+
+@Injectable()
+export class RecipeData {
+    private db: AngularFireDatabase;
+    private firebaseCollection: FirebaseListObservable<any[]>;
+    public items;
+
+    constructor(db: AngularFireDatabase) {
+        this.db = db;
+        this.firebaseCollection = this.db.list('/recipes');
+    }
+
+    getAll() {
+        this.firebaseCollection.subscribe(items => {
+            this.items = items;
+        });
+
+        return this.items;
+    }
+
+    // need to add some notifications, not console outputs
+    add(recipe: RecipeInterface): void {
+        this.firebaseCollection.push(recipe)
+            .then(_ => console.log('recipe added'))
+            .catch(err => console.log(err, 'err when adding recipe'));
+    }
+
+    getRecipeByTitle(title: string) {
+        const items = this.db.list('recipes', {
+            preserveSnapshot: true,
+        });
+
+        let item: any;
+
+        // need to be upgraded
+        items.subscribe(snapshots => {
+            snapshots.forEach(snapshot => {
+                if (snapshot.val().title === title) {
+                    item = snapshot.val();
+                }
+            });
+        });
+
+        return item;
+    }
+}
