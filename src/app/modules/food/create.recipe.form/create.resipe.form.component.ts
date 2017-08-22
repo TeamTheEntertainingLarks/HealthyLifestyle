f203fdf272c6c36517f1b78cbf09bfaab7ab720c
lorashopova <lshopowa@yahoo.com>
2017-08-22 16:30:02 +0300

    form control validators for create form added

--- src/app/modules/food/create.recipe.form/create.resipe.form.component.ts
+++ src/app/modules/food/create.recipe.form/create.resipe.form.component.ts
@@ -27,9 +27,16 @@ export class CreateFormComponent implements OnInit {
   categories = ['breakfast', 'soups', 'salads', 'desserts', 'breads', 'main dishes', 'side dishes'];
 
   public recipeForm: FormGroup;
+
   public titleFormControl: AbstractControl;
   public authorFormControl: AbstractControl;
+  public categoryFormControl: AbstractControl;
   public descriptionFormControl: AbstractControl;
+  public ingredientsFormControl: AbstractControl;
+  public stepFirstFormControl: AbstractControl;
+  public stepSecondFormControl: AbstractControl;
+  public stepThirdFormControl: AbstractControl;
+  public imageFormControl: AbstractControl;
 
   constructor(private recipedata: RecipeData, private formBuilder: FormBuilder) {
      this.recipe = new Recipe();
@@ -38,9 +45,43 @@ export class CreateFormComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.titleFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.authorFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.categoryFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.descriptionFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.ingredientsFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.stepFirstFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.stepSecondFormControl = new FormControl();
+
+    this.stepThirdFormControl = new FormControl();
+
+    this.imageFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.recipeForm = this.formBuilder.group({
+      titleFormControl: this.titleFormControl,
+      authorFormControl: this.authorFormControl,
+      categoryFormControl: this.categoryFormControl,
+      descriptionFormControl: this.descriptionFormControl,
+      ingredientsFormControl: this.ingredientsFormControl,
+      stepFirstFormControl: this.stepFirstFormControl,
+      stepSecondFormControl: this.stepSecondFormControl,
+      stepThirdFormControl: this.stepThirdFormControl,
+      imageFormControl: this.imageFormControl
+    });
   }
-
-
 
   onSubmit() {
   }
