3b9525e716f98fe4e80cff3d13b76c19e3468c68
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-08-25 17:07:09 +0300

    signIn form styles changed. Verification changed.

--- src/app/modules/user/signIn-form/signIn-form.component.ts
+++ src/app/modules/user/signIn-form/signIn-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
-import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 
 @Component({
   selector: 'app-signin-form',
@@ -8,71 +11,36 @@ import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angula
   styleUrls: ['./signIn-form.component.css']
 })
 export class SignInFormComponent implements OnInit {
+  public email;
+  public password;
+  public userForm: FormGroup;
+  public emailFormControl: AbstractControl;
+  public passwordFormControl: AbstractControl;
 
-  userForm: FormGroup;
-
-  formErrors = {
-    'email': '',
-    'password': ''
-  };
-
-  validationMessages = {
-    'email': {
-      'required': 'Email is required.',
-      'email': 'Email must be a valid email'
-    },
-    'password': {
-      'required': 'Password is required.',
-      'pattern': 'Password must be include at one letter and one number.',
-      'minlength': 'Password must be at least 4 characters long.',
-      'maxlength': 'Password cannot be more than 40 characters long.',
-    }
-  };
-
-  constructor(private fb: FormBuilder, private auth: AuthService) { }
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) { }
 
   ngOnInit(): void {
-    this.buildForm();
+    this.createForm();
   }
 
-  buildForm(): void {
-    this.userForm = this.fb.group({
-      'email': ['', [
-        Validators.required,
-        Validators.email
-      ]
-      ],
-      'password': ['', [
-        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
-        Validators.minLength(6),
-        Validators.maxLength(25)
-      ]
-      ],
-    });
+  createForm() {
+    this.emailFormControl = new FormControl('', [
+      Validators.required,
+      Validators.pattern(EMAIL_REGEX)]);
 
-    this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
-    this.onValueChanged(); // reset validation messages
+      this.passwordFormControl = new FormControl('', [
+        Validators.required,
+        Validators.pattern(PASSWORD_REGEX),
+        Validators.minLength(6),
+        Validators.maxLength(25)]);
+
+    this.userForm = this.formBuilder.group({
+      emailFormControl: this.emailFormControl,
+      passwordFormControl: this.passwordFormControl,
+    });
   }
 
   signIn(): void {
-    this.auth.emailLogin(this.userForm.value['email'], this.userForm.value['password']);
-  }
-
-  onValueChanged(data?: any) {
-    if (!this.userForm) { return; }
-    const form = this.userForm;
-    // tslint:disable-next-line:forin
-    for (const field in this.formErrors) {
-      // clear previous error message (if any)
-      this.formErrors[field] = '';
-      const control = form.get(field);
-      if (control && control.dirty && !control.valid) {
-        const messages = this.validationMessages[field];
-        // tslint:disable-next-line:forin
-        for (const key in control.errors) {
-          this.formErrors[field] += messages[key] + ' ';
-        }
-      }
-    }
+    this.auth.emailLogin(this.email, this.password);
   }
 }
