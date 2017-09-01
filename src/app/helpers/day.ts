b2708bee1d83a26357bc8b02c75b9a1a94c4ae7f
Yordan <dadanchi@abv.bg>
2017-09-01 19:44:26 +0300

    removed helper classess....didnt need them for the expected output

--- src/app/helpers/day.ts
+++
@@ -1,9 +0,0 @@
-import { Workout } from './../models/workout';
-export class Day {
-    constructor(workout: any, checked: boolean = false) {
-        this.workout = workout;
-        this.checked = checked;
-    }
-    public workout: any;
-    public checked: boolean;
-}
