50462b256b0510881d80fb74f494ac7c9dad7cf9
lorashopova <lshopowa@yahoo.com>
2017-09-04 00:31:07 +0300

    nutrition service added

--- src/app/modules/nutrition/nutrition/nutrition.component.ts
+++ src/app/modules/nutrition/nutrition/nutrition.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NutritionInterface } from '../../../interfaces/nutrition';
+import { NutritionData } from '../../../services/nutrition-data.service';
+import { AuthService } from '../../../services/auth.service';
+
 @Component({
   selector: 'app-nutrition',
   templateUrl: './nutrition.component.html',
   styleUrls: ['./nutrition.component.css']
 })
 export class NutritionComponent implements OnInit {
+  public nutrition: Array<NutritionInterface>;
+  public meal;
+  public article;
 
-  constructor() { }
+  constructor(public nutritonDataService: NutritionData, public auth: AuthService) { }
 
   ngOnInit() {
+    this.nutritonDataService.getAllArticles().subscribe(items => {
+      this.article = items;
+      console.log(this.article);
+    });
+
+    this.nutritonDataService.getAllMeals().subscribe(items => {
+      this.meal = items;
+      console.log(this.meal);
+    });
+
   }
 
 }
