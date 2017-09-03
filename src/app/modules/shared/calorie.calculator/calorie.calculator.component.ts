25871be6b8edcc6a3e5eda2205647cb1540f380a
lorashopova <lshopowa@yahoo.com>
2017-09-03 13:34:02 +0300

    calorie calculator formControl added

--- src/app/modules/shared/calorie.calculator/calorie.calculator.component.ts
+++ src/app/modules/shared/calorie.calculator/calorie.calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
@@ -7,30 +7,77 @@ import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from
   styleUrls: ['./calorie.calculator.component.css']
 })
 export class CalorieCalculatorComponent implements OnInit {
+  public calculatorForm: FormGroup;
+  public age: number;
+  public genderValue: string;
+  public weight: number;
+  public height: number;
+  public activenessValue: string;
   result;
-  selectedValue;
   gender = [
-    {value: 'male', viewValue: 'Male'},
-    {value: 'female', viewValue: 'Female'}
+    { value: 'male', viewValue: 'Male' },
+    { value: 'female', viewValue: 'Female' }
   ];
-  activeness = ['Sedentary', 'Light Active', 'Moderately Active', 'Very Active', 'Extra Active'];
+  activeness = [
+    { value: 'sedentary', viewValue: 'Sedentary' },
+    { value: 'lightActive', viewValue: 'Light Active' },
+    { value: 'moderatelyActive', viewValue: 'Moderately Active' },
+    { value: 'veryActive', viewValue: 'Very Active' },
+    { value: 'extraActive', viewValue: 'Extra Active' }];
 
-  constructor() { }
+  public ageFormControl: AbstractControl;
+  public genderFormControl: AbstractControl;
+  public weightFormControl: AbstractControl;
+  public heightFormControl: AbstractControl;
+  public activenessFormControl: AbstractControl;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ageFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.genderFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.weightFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.heightFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.activenessFormControl = new FormControl('', [
+      Validators.required]);
+
+    this.calculatorForm = this.formBuilder.group({
+      ageFormControl: this.ageFormControl,
+      genderFormControl: this.genderFormControl,
+      weightFormControl: this.weightFormControl,
+      heightFormControl: this.heightFormControl,
+      activenessFormControl: this.activenessFormControl
+    });
   }
 
-  calculate(age, selectedValue, weight, height) {
+  calculate(age, genderValue, weight, height, activenessValue) {
     let calorie = 0;
-    if (selectedValue === 'male') {
+    if (genderValue === 'male') {
       calorie = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
     } else {
-      calorie = 655 + (9.6 * weight ) + (1.8 * height) - (4.7 * age);
+      calorie = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
     }
 
-    this.result = `You need rouhgly ${calorie} calories a day to maintain your current weight.`;
+    this.result = `You need rouhgly ${calorie} calories a day.`;
 
-    // console.log(age, selectedValue, weight, height);
+    //  console.log(age, genderValue, weight, height, activenessValue);
 
+  }
+
+  clearCalculatorForm(age, genderValue, weight, height, activenessValue) {
+    this.genderValue = ' ';
+    this.age = 0;
+    this.genderValue = ' ';
+    this.weight = 0;
+    this.height = 0;
+    this.activenessValue = ' ';
   }
 }
