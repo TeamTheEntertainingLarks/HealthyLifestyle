1f3d25c86830cd581c28104b58b882bcbc72dddd
lorashopova <lshopowa@yahoo.com>
2017-09-10 07:05:41 +0300

    articles and recipes to user collection added

--- src/app/modules/user/user-collection/user-collection.component.ts
+++ src/app/modules/user/user-collection/user-collection.component.ts
@@ -2,6 +2,8 @@ import { ActivityInterface } from './../../../interfaces/activity';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { ActivityData } from '../../../services/activity-data.service';
+import { NutritionData } from './../../../services/nutrition-data.service';
+import { RecipeData } from './../../../services/recipe-data.service';
 
 @Component({
   selector: 'app-user-collection',
@@ -14,10 +16,14 @@ export class UserCollectionComponent implements OnInit {
   public activities;
   public joinedActivities;
   public createdActivities;
+  public createdArticles;
+  public createdRecipes;
 
   constructor(
     private auth: AuthService,
-    private activitiesDataService: ActivityData) { }
+    private activitiesDataService: ActivityData,
+    private articleDataService: NutritionData,
+    private recipeDataService: RecipeData) { }
 
   ngOnInit() {
     this.userId = localStorage.getItem('authkey');
@@ -34,6 +40,20 @@ export class UserCollectionComponent implements OnInit {
               return item.participants.indexOf(this.userId) !== -1;
             }
           });
+      });
+
+    this.articleDataService
+      .getAllArticles()
+      .subscribe(items => {
+        this.createdArticles =
+          items.filter(item => item.userId === this.userId);
+      });
+
+    this.recipeDataService
+      .getAllRecipes()
+      .subscribe(items => {
+        this.createdRecipes =
+          items.filter(item => item.userId === this.userId);
       });
   }
 }
